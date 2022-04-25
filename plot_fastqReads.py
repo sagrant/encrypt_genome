7c725a1edfda504763b4bc9a667bdf20cdefb735
Sadie Grant <sagrant@ucsc.edu>
2022-04-25 12:51:42 -0700

    working plot FASTQ reads

---
+++ plot_fastqReads.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+inFile = 'shared_reads.csv'
+
+with open(inFile, 'r') as file: 
+    df = pd.read_csv(file, usecols = [0], squeeze = True, engine = 'c')
+
+data = df.values
+fig, ax = plt.subplots()
+plt.hist(data, bins = 20, color = 'black', log = True, zorder = 20)
+
+ax.set_yticks([10e5, 10e6, 10e7, 10e8])
+ax.set_xticks([1, 2, 3, 4, 5, 6])
+ax.tick_params(axis = 'both', which = 'both', length = 1)
+ax.set_ylabel('Shared Reads (#)')
+ax.set_xlabel('People (#)')
+
+plt.grid(b = True, which = 'minor', axis = 'both', zorder = 10)
+plt.yticks([10e5, 10e6, 10e7, 10e8])
+
+plt.title('Number of Shared Reads in Merged FASTQ File')
+plt.show() #view figure
+#plt.savefig('shared_reads_fastq.png', dpi = 600) #uncomment to save figure 
