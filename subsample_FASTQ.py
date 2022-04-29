633a8ef85f38ef78c6348f9ade94b4c96e462add
Sadie Grant <sagrant@ucsc.edu>
2022-04-29 07:49:48 -0700

    added annotations

--- subsample_FASTQ.py
+++ subsample_FASTQ.py
@@ -4,7 +4,8 @@ import itertools
 import numpy as np
 import sys
 
-'''Input: any amount of paired end FASTQ files
+'''
+Input: any amount of paired-end FASTQ files
 Output: FASTQ files subsampled to 300,000,000 reads'''
 
 parser = argparse.ArgumentParser()
@@ -26,16 +27,17 @@ class subsample():
     def execute(self):
         compile1 = []
         compile2 = []
-        for line in range(0, len(self.content1), 4):
+        #for loop to store ID, seq, and quality score of each read in nested list
+        for line in range(0, len(self.content1), 4): 
             forward = self.content1[line: line+4]
             reverse = self.content2[line: line+4]
             compile1.append(forward)
             compile2.append(reverse)
 
         random.seed(42)
-        random_1 = random.sample(compile1, 40000)
+        random_1 = random.sample(compile1, 30000000) 
         random.seed(42)
-        random_2 = random.sample(compile2, 40000)
+        random_2 = random.sample(compile2, 30000000)
         combo1 = list(itertools.chain(*random_1))
         combo2 = list(itertools.chain(*random_2))
         return combo1, combo2 
@@ -53,8 +55,8 @@ def main():
           
     process = subsample(fastq1, fastq2)
     combo1, combo2 = process.execute()
-    
 
+    #write concatenated paired end FASTQs to new file
     with open(args.outFile_1, 'w') as out1:
         out1.write('\n'.join(str(i) for i in combo1))
 
