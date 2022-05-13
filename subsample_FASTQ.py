c660f4e61faf9a8f564f89fe0edd1e8ffd6ea4df
Sadie Grant <sagrant@ucsc.edu>
2022-05-13 09:44:24 -0700

    stores nested read list differently, fixed final no. reads

--- subsample_FASTQ.py
+++ subsample_FASTQ.py
@@ -2,7 +2,6 @@ import argparse
 import random
 import itertools
 import numpy as np
-import sys
 
 '''
 Input: any amount of paired-end FASTQ files
@@ -12,58 +11,48 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-i1', '--filename1') #forward strand
 parser.add_argument('-i2', '--filename2') #reverse strand
-parser.add_argument('-fq1', '--outFile_1') #forward strands merged and subsampled
-parser.add_argument('-fq2', '--outFile_2') #reverse strands merged and subsampled
+parser.add_argument('-fq1', '--outFile_1') #forward strands concatenated and subsampled
+parser.add_argument('-fq2', '--outFile_2') #reverse strands concatenated and subsampled
 
 args = parser.parse_args()
 
 
-class subsample():
+def process(file):
+    fullFq = []
+    with open(file, 'r') as fileContent:
+        for idx, line in enumerate(fileContent): #for loop to store each ID, read, and quality score in nested list 
+            if idx % 4 == 0: 
+                subsampleLines = []
+                subsampleLines.append(line)
+            elif idx % 3 == 0: 
+                subsampleLines.append(line)
+            elif idx % 2 == 0:
+                subsampleLines.append(line)
+            elif idx % 1 == 0:
+                subsampleLines.append(line)
+            fullFq.append(subsampleLines)
+            
 
-    def __init__(self, file1, file2):
-        self.content1 = file1
-        self.content2 = file2
-
-    def execute(self):
-        compile1 = []
-        compile2 = []
-        #for loop to store ID, seq, and quality score of each read in nested list
-        for line in range(0, len(self.content1), 4): 
-            forward = self.content1[line: line+4]
-            reverse = self.content2[line: line+4]
-            compile1.append(forward)
-            compile2.append(reverse)
-
-        random.seed(42)
-        random_1 = random.sample(compile1, 30000000) 
-        random.seed(42)
-        random_2 = random.sample(compile2, 30000000)
-        combo1 = list(itertools.chain(*random_1))
-        combo2 = list(itertools.chain(*random_2))
-        return combo1, combo2 
+    random.seed(42)
+    randomFq = random.sample(fullFq, 30000000) #subsample to 30,000,000 reads 
+    compileFq = list(itertools.chain(*randomFq))
+    return compileFq
 
 
 def main():
-
-    with open(args.filename1, 'r') as file_1:
-        f1 = file_1.read()
-        fastq1 = f1.split('\n')
-
-    with open(args.filename2, 'r') as file_2: 
-        f2 = file_2.read()
-        fastq2 = f2.split('\n')
           
-    process = subsample(fastq1, fastq2)
-    combo1, combo2 = process.execute()
+    processFq1 = process(args.filename1)
+    processFq2 = process(args.filename2)
+
+    fq1 = np.asarray(processFq1).flatten()
+    fq2 = np.asarray(processFq2).flatten()
 
     #write concatenated paired end FASTQs to new file
     with open(args.outFile_1, 'w') as out1:
-        out1.write('\n'.join(str(i) for i in combo1))
+        out1.write(''.join(fq1))
 
     with open(args.outFile_2, 'w') as out2:
-        out2.write('\n'.join(str(i) for i in combo2))
-
-    sys.exit()
+        out2.write(''.join(fq2))
 
 if __name__ == "__main__":
     main()
