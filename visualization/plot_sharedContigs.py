a058fac09cc6091be88541c5ca1347893c1567c2
Sadie Grant <sagrant@ucsc.edu>
2022-05-18 11:39:25 -0700

    added plot ticks & lables

--- visualization/plot_sharedContigs.py
+++ visualization/plot_sharedContigs.py
@@ -7,27 +7,34 @@ import matplotlib.pyplot as plt
 Plot number of reads shared between people in concatenated paired-end FASTQ files.
 '''
 
-inFile = '/home/sagrant/encrypt_genome/data/shared_contigs.csv'
-#inFile = 'shared_contigs.csv'
+class plotShared():
+    
+    def __init__(self, s):
+        self.contigSeries = s
 
-with open(inFile, 'r') as file: 
-    df = pd.read_csv(file, usecols = [0], squeeze = True, engine = 'c')
+    def plotHist(self):
+        fig, ax = plt.subplots()
+        plt.hist(self.contigSeries, log = True, density = True, color = 'black', zorder = 20)
+        ax.set_ylabel('Contigs (#)')
+        ax.set_xlabel('People (#)')
 
-contigData = df.values[1:-1]
+        ax.set_xticks([1, 2, 3, 4, 5, 6, 7, 8])
+        ax.tick_params(axis = 'both', which = 'both', length = 1)
 
-fig, ax = plt.subplots()
-plt.hist(contigData, log = True, density = True, color = 'black', zorder = 20)
+        plt.xlim(0.8, 8.7)
+        plt.grid(b = True, which = 'minor', axis = 'both', zorder = 10)
+        plt.title('Number of Shared Contigs in Merged Assembly')
+        plt.show()
 
-# ax.set_ylabel('Contigs (#)')
-# ax.set_xlabel('People (#)')
-# #ax.set_xticks([1, 2, 3, 4, 5, 6, 7, 8])
-# ax.set_xticks([1, 2, 3, 4, 5, 6, 7, 8])
-# ax.tick_params(axis = 'both', which = 'both', length = 1)
+def main():
+    
+    inFile = '/home/sagrant/encrypt_genome/data/shared_contigs.csv'
 
-# plt.xlim(0.8, 8.7)
-# plt.grid(b = True, which = 'minor', axis = 'both', zorder = 10)
-# plt.title('Number of Shared Contigs in Merged Assembly')
-plt.show()
-#plt.savefig('shared_person2contig.png', dpi = 600)
-#plt.savefig('shared_person2contig_lc.png', dpi = 600)
-#plt.savefig('shared_person2contig_6merged.png', dpi = 600)
+    with open(inFile, 'r') as file: 
+        df = pd.read_csv(file, usecols = [0], squeeze = True, engine = 'c')
+
+    contigData = df.values[1:-1]
+    graph = plotShared(contigData)
+    plotShared.plotHist(graph)
+if __name__ == "__main__":
+    main()
