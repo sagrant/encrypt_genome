4a34653e51a1281cafe8fc59c2f65a780a1220b5
Sadie Grant <75453911+sagrant@users.noreply.github.com>
2025-03-29 17:27:45 -0700

    Simplified code and removed notes to self

--- automated_scripts/automate_assembly.sh
+++ automated_scripts/automate_assembly.sh
@@ -2,23 +2,6 @@
 #assembly_control.sh 
 #automates assmebly process to generate control assemblies 
 
-#change once working in github directory (encrypt_genome)
-# concat1=~/encrypt_genome/concat_1.fastq
-# concat2=~/encrypt_genome/concat_2.fastq
-
-# cd SPAdes-3.15.4-Linux/bin/
-# mkdir concat_spades_out/
-# cd ..
-# cd ..
-# output_dir=/home/sagrant/encrypt_genome/SPAdes-3.15.4-Linux/bin/concat_spades_out/
-
-# (cd ./SPAdes-3.15.4-Linux/bin && exec ./spades.py -1 $concat1 -2 $concat2 -o $output_dir)
-# ./minimap2/minimap2 -ax sr $output_dir/contigs.fasta $concat1 $concat2 > concat.sam
-
-#working in dna_privacy
-#export $PATH=PATH:/home/sagrant/python/bin/python3.9
-
-
 mkdir /home/sagrant/dna_privacy/SPAdes-3.15.4-Linux/bin/concat_spades_out
 output_dir=~/home/sagrant/dna_privacy/SPAdes-3.15.4-Linux/bin/concat_spades_out
 
